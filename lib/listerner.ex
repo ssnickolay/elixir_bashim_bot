ac7ba9fbcabc990d446d77f3eb67fb43f8ec0101
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-07-12 19:09:16 +0300

    compile!

--- lib/listerner.ex
+++ lib/listerner.ex
@@ -4,9 +4,9 @@ defmodule Listener do
   def listen(offset \\ 0, callback) do
     case Nadia.get_updates(limit: 10, timeout: 10, offset: offset) do
       {:ok, updates} ->
-        handle_updates(updates)
+        handle_updates(updates, offset, callback)
       {:error, error} ->
-        Logger.debug(&(inspect(error)))
+        Logger.debug fn -> inspect(error) end
         listen(offset, callback)
     end
   end
@@ -14,7 +14,7 @@ defmodule Listener do
   defp handle_updates([], offset, callback), do: listen(offset, callback)
   defp handle_updates(updates, _offset, callback) do
     Enum.each(updates, &callback.(&1))
-    max = Enum.max_by(upds, fn(upd) -> updates.update_id end, fn -> 0 end).update_id
+    max = Enum.max_by(updates, &(&1.update_id), fn -> %{update_id: 0} end).update_id
     listen(max + 1, callback)
   end
 end
