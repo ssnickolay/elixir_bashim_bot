6725c7a7f4d05c37a5b8faecbeff3d8c19bc7e6f
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-07-12 19:02:43 +0300

    dont use case in case

--- lib/listerner.ex
+++ lib/listerner.ex
@@ -4,17 +4,17 @@ defmodule Listener do
   def listen(offset \\ 0, callback) do
     case Nadia.get_updates(limit: 10, timeout: 10, offset: offset) do
       {:ok, updates} ->
-        case updates do
-          [] -> listen(offset, callback)
-          upds ->
-            Enum.each(upds, &callback.(&1))
-            max = Enum.max_by(upds, fn(upd) -> upd.update_id end, fn -> 0 end).update_id
-            listen(max + 1, callback)
-        end
-
+        handle_updates(updates)
       {:error, error} ->
-        Logger.debug fn -> inspect(error) end
+        Logger.debug(&(inspect(error)))
         listen(offset, callback)
     end
+  end
+
+  defp handle_updates([], offset, callback), do: listen(offset, callback)
+  defp handle_updates(updates, _offset, callback) do
+    Enum.each(updates, &callback.(&1))
+    max = Enum.max_by(upds, fn(upd) -> updates.update_id end, fn -> 0 end).update_id
+    listen(max + 1, callback)
   end
 end
